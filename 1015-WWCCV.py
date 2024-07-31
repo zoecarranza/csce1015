00230a9044b46deb560a35073f7da5113c6415e3
Ryan Garlick <garlick@unt.edu>
2024-07-30 23:50:49 -0500

    Add files via upload

---
+++ 1015-WWCCV.py
@@ -0,0 +1,23 @@
+# This is the world's worst credit card number validator. It checks to see if the number is 16 digits long and that is it.  
+
+# anything that starts with a pound sign (hashtag if you are under 30) is a comment. The computer ignores these.
+
+# Let's tell the world whose awful credit card validator this is.
+print ("Joe Dirt's Credit Card Validator")
+
+# Let's get the card number from the user
+card_number = input("Enter your 16-digit credit card number: ")
+
+# Check if card number is 16 digits long
+
+# len means 'length', and the '==' is testing one thing against the other. A single equal sign would set one thing equal to the other. 
+# card_number.isdigit() is checking if this is numbers instead of someone typing 'cheeseburger' for their credit card number.
+
+if len(card_number) == 15 and card_number.isdigit():     
+
+    print ("Card is valid.")
+
+else:
+
+    print ("Invalid card number. It must be 16 digits long.")
+    
