eab4bade8b1c911fc18bbcbbcb5bd661e7364479
Ryan Garlick <garlick@unt.edu>
2024-07-30 23:52:17 -0500

    Update 1015-WWCCV.py

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -3,14 +3,14 @@
 # anything that starts with a pound sign (hashtag if you are under 30) is a comment. The computer ignores these.
 
 # Let's tell the world whose awful credit card validator this is.
-print ("Joe Dirt's Credit Card Validator")
+print ("Joe Dirt Modified This Credit Card Validator")
 
 # Let's get the card number from the user
 card_number = input("Enter your 16-digit credit card number: ")
 
 # Check if card number is 16 digits long
 
-# len means 'length', and the '==' is testing one thing against the other. A single equal sign would set one thing equal to the other. 
+# len means 'length', and the '==' is testing one thing against the other. A single equal sign would set one thing equal to the other (not what we want to do). 
 # card_number.isdigit() is checking if this is numbers instead of someone typing 'cheeseburger' for their credit card number.
 
 if len(card_number) == 15 and card_number.isdigit():     
